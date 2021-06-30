d43ba45bb83f11466f603fbe6c5ec2b52227cf53
tirelyl <yulunli.lyl@alibaba-inc.com>
2021-06-30 19:44:01 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = new express();
+
+const sleepFun = (time) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, time)
+  });
+};
+
+const filter = (req, res, next) => {
+  const { sleep } = req.query || 0;
+  if (sleep) {
+    sleepFun(sleep).then(() => next());
+  } else {
+    next();
+  }
+};
+
+app.use(filter);
+
+app.use('/static/', express.static('./static/'));
+
+const port = 3000;
+
+app.get('/', (req, res, next) => {
+  fs.readFile('./index.html', 'UTF-8', (err, data) => {
+    if (err) return;
+    res.send(data);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`app is running at http://127.0.0.1:${port}`);
+});
